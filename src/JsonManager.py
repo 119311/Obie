ec7d0d94e38e563b8363b02b0b84325510102b35
adobesan <adobeoftheocean@outlook.jp>
2022-07-10 23:59:20 +0900

    add save to do (add update num)

--- src/JsonManager.py
+++ src/JsonManager.py
@@ -1,18 +1,32 @@
 import unittest  # testのためのライブラリ
 import json
+import pprint
+import collections
 
 
 JSON_PATH = "data/target.json"
+JSON_PATH_W = "data/target_w.json"
 
 
 def load(jsonFile=JSON_PATH):
     """Load json from jsonFile to dict. (default is JSON_PATH)"""
-    return json.load(open(jsonFile, "r", encoding="utf-8_sig"))
+    return json.load(
+        open(jsonFile, "r", encoding="utf-8_sig"),
+    )
 
 
-def save(data):
-    with open(JSON_PATH, "w", encoding="utf-8_sig") as f:
-        json.dump(data, f, indent=4)
+def save(data, jsonFile=JSON_PATH_W, mode="a"):
+    """
+    Damp to json from data(dict).
+    mode="a" is append mode.
+    mode="o" is overwrite mode.
+    """
+    dataOld = load()
+    dataOld["targetList"].append(data)
+    # pprint.pprint(dataOld)
+    # print(json.dumps(dataOld, ensure_ascii=False, indent=2))
+    with open(JSON_PATH_W, mode="wt", encoding="utf-8") as file:
+        json.dump(dataOld, file, ensure_ascii=False, indent=4)
 
 
 def getByDictionary(id, Dictionary):
@@ -34,8 +48,19 @@ test = {
         "active": None,
     },
 }
+test2 = {
+    "id": 4,
+    "attribute": {
+        "kind": "食事",
+        "amount": 12121,
+        "Period": None,
+        "repeat": None,
+        "active": None,
+    },
+}
 
 
 print(test)
 getByDictionary(test.get("id"), test)
 print(test)
+save(test2)
